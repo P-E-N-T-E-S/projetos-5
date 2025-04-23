e2dfb94bb18c5a2b789ba31e7d13120e2e620114
Sofia-Saraiva <sofia19lima@gmail.com>
2025-04-23 19:30:51 -0300

    comentarioService quase pronto

---
+++ dominio/src/main/java/com/capibyte/acervo/dominio/core/opiniao/comentario/ComentarioService.java
@@ -0,0 +1,28 @@
+package com.capibyte.acervo.aplicacao.opiniao;
+
+import com.capibyte.acervo.dominio.core.opiniao.comentario.Comentario;
+import com.capibyte.acervo.dominio.core.opiniao.comentario.ComentarioRepository;
+import com.capibyte.acervo.dominio.core.acervo.livro.Livro;
+import com.capibyte.acervo.dominio.core.administracao.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ComentarioService {
+
+    private final ComentarioRepository comentarioRepository;
+
+    public ComentarioService(ComentarioRepository comentarioRepository) {
+        this.comentarioRepository = comentarioRepository;
+    }
+
+    public void adicionarComentario(String isbnLivro, Livro livro, Usuario usuario, String conteudo) throws Exception {
+        Comentario comentario = Comentario.criar(livro, usuario, conteudo);
+        comentarioRepository.salvar(comentario);
+    }
+
+    public List<Comentario> listarComentarios(String isbnLivro) {
+        return comentarioRepository.listarPorIsbn(isbnLivro);
+    }
+}
--- dominio/src/main/java/com/capibyte/acervo/dominio/core/opiniao/comentario/ComentarioServico.java
+++
@@ -1,8 +0,0 @@
-package com.capibyte.acervo.dominio.core.opiniao.comentario;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ComentarioServico {
-
-}
