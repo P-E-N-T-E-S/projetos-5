22605aec1cb5d77b518340cc62d6138d0169489f
Sofia-Saraiva <sofia19lima@gmail.com>
2025-05-30 22:12:34 -0300

    ajeitando auth de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,17 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from "./pages/Login.jsx";
+import PrivateRoute from "./services/PrivateRoute.jsx";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+
+                <Route element={<PrivateRoute />}>
+                    <Route path="/" element={<Home />} />
+                </Route>
             </Routes>
         </Router>
     );
